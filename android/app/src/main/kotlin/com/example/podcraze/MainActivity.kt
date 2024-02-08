616d10388718405dc8c646cde3e02e5c4bbcd3f1
nirdesh17 <nirdeshdevadiya17@gmail.com>
2024-02-08 22:03:39 +0530

    Slash Screen & Onboarding Screen Added

---
+++ android/app/src/main/kotlin/com/example/podcraze/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.podcraze
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model_intro.dart
@@ -0,0 +1,22 @@
+class IntroContent {
+  String image;
+  String description;
+  IntroContent({
+    required this.image,
+    required this.description,
+  });
+}
+
+List<IntroContent> contents=[
+  IntroContent(
+    image: 'assets/images/2_Light_welcome screen4x.png', 
+    description: "Welcome to Podcraze, the best podcast app for you."),
+  IntroContent(
+    image: 'assets/images/2_Light_welcome screen4x.png',
+    description: "Discover and listen to your favorite podcasts."),
+  IntroContent(
+    image: 'assets/images/2_Light_welcome screen4x.png',
+    description: "Create your own playlist and enjoy your favorite podcasts."),
+];
+
+
