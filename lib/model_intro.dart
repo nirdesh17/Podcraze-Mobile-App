d8a82faf263eee0e7dc6cd27a5c2764ec72a4090
nirdesh17 <nirdeshdevadiya17@gmail.com>
2024-02-09 12:22:48 +0530

    updates

--- lib/model_intro.dart
+++ lib/model_intro.dart
@@ -9,13 +9,13 @@ class IntroContent {
 
 List<IntroContent> contents=[
   IntroContent(
-    image: 'assets/images/2_Light_welcome screen4x.png', 
+    image: 'assets/images/onboarding 1.png', 
     description: "Welcome to Podcraze, the best podcast app for you."),
   IntroContent(
-    image: 'assets/images/2_Light_welcome screen4x.png',
+    image: 'assets/images/onboarding 2.png',
     description: "Discover and listen to your favorite podcasts."),
   IntroContent(
-    image: 'assets/images/2_Light_welcome screen4x.png',
+    image: 'assets/images/onboarding 3.png',
     description: "Create your own playlist and enjoy your favorite podcasts."),
 ];
 
