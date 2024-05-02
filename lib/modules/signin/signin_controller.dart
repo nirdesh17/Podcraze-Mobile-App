384be8dfd5892a23a2627a7e9ebea3de54c90dca
nirdesh17 <nirdeshdevadiya17@gmail.com>
2024-05-02 12:09:13 +0530

    Sign In routes updated

--- lib/modules/signin/signin_controller.dart
+++ lib/modules/signin/signin_controller.dart
@@ -45,13 +45,28 @@ class SigninController extends GetxController {
 
       var response =
           await signInApi.signIn(signInPostBodyModel: signInPostBodyModel);
+
+      
       if (response.code == 200 || response.code == 210) {
+
+        
         GetStorage().remove(KEY_IS_API_ERROR_HANDLE);
         await localStorage.write(KEY_USER_DATA, response.toJson());
         isLoading.value = false;
-        Get.offNamed(
-          AppRoutes.interestCategoryScreen,
-        );
+        var userdata = response.toJson();
+        var userInterest = userdata["result"]["user_interests"];
+        var subscribedShow = userdata["result"]["subscribed_shows"];
+        if (userInterest == null || userInterest.isEmpty) {
+          Get.offNamed(AppRoutes.interestCategoryScreen);
+          // print("No interest");
+        } else if (subscribedShow == null || subscribedShow.isEmpty) {
+          Get.offNamed(AppRoutes.subscribeShowScreen);
+        } else {
+          Get.offNamed(AppRoutes.homeScreen);
+        }
+        // Get.offNamed(
+        //   AppRoutes.interestCategoryScreen,
+        // );
         AppUtils.showSnackBar("Sign In Succesful",
             status: MessageStatus.SUCCESS);
       } else {
